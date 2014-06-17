1e33c78db8f524d3a5c886afc56ed0a2c570e487
John Lau <johnlau@Johns-MacBook-Air.local>
2014-06-18 01:06:09 +0800

    change

--- secureme/index.php
+++ secureme/index.php
@@ -16,7 +16,7 @@
 
     <!-- Custom styles for this template -->
     <link href="assets/style.css" rel="stylesheet">
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
     <script>
       $(function() {
@@ -88,7 +88,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>  
+      
     <script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
